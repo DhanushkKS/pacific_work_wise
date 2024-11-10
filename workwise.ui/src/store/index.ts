13766e7120ff21efd5df8ea5ef48bcca6ca675de
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-11-10 22:18:29 +0530

    Configured redux store and folder structure in ui

---
+++ workwise.ui/src/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer.js";
+import { rootMiddleware } from "./rootMiddleware.js";
+import { useDispatch as useReduxDispatch } from "react-redux";
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat([...rootMiddleware]),
+});
+export const useDispatch = () => useReduxDispatch();
